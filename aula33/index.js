88ed7504c156de2fa5db90997c729579d39e31b4
luan <luanmesias1211@gmail.com>
2024-11-01 21:04:56 -0300

    Mais aulas

---
+++ aula33/index.js
@@ -0,0 +1,14 @@
+//const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+//
+////const [um, dois, tres] = numeros;
+//const [um, , tres, , cinco, , sete] = numeros;
+//
+//console.log(um, tres, cinco);
+
+const num = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+
+//const [,[,,seis]] = num;
+
+const [lista1, lista2, lista3] = num;
+
+console.log(lista2[2]);
